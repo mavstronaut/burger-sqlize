0f8cff36bb614b8381a8f5e102300b889fbb5f7d
mavstronaut <mavbarona@gmail.com>
2019-08-15 21:01:28 -0400

    updating connection and schema

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,20 @@ var connection;
 require('dotenv').config();
 var newLine = '\n\n';
 
-if (process.env.JAWSDB_URL) { 
-    // heroku database
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // running on localhost
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: process.env.sqlPass,
-        database: 'burger_db'
-    });
-};
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('ERROR: MySQL connection error -- ' + err.stack + newLine);
-        return;
+// heroku database
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+
+var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+    host: selectedSource.host,
+    dialect: 'mysql',
+  
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
     }
-    console.log('Connected to MySQL database as id ' + connection.threadId + newLine);
 });
 
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE burger_db;
-USE burger_db;
-
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
