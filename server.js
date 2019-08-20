11a0a6e73cf0710092276fb2d739213eb9043704
mavstronaut <mavbarona@gmail.com>
2019-08-20 17:46:47 -0400

    updating server.js for sequelize

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var port = process.env.PORT || 3030;
-var app = express();
+var exphbs = require('express-handlebars');
+var log = require("loglevel");
+var path = require("path");
 
+// Configure logging
+console.log('process.env.NODE_ENV (in server.js) = ' + process.env.NODE_ENV);
+if (process.env.NODE_ENV) { // Production
+	console.log("Setting log level to ERROR");
+	log.setLevel("ERROR");
+} else { // Development
+	var level = process.env.LOG_LEVEL || "DEBUG";
+	console.log("Setting log level to " + level);
+	log.setLevel(level);
+}
+
+// Set up Express Application
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require(path.join(__dirname, '/models'));
+
+// Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=PUT
 app.use(methodOverride('_method'));
 
-var exphandlebars = require('express-handlebars');
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.engine('handlebars', exphandlebars({ defaultLayout: 'main' }));
+// Set Handlebars as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burgers_controller.js');
+// Import routes and give the server access to them
+require('./controllers/burgers_controller.js')(app);
 
-app.use('/', routes);
-
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+// Include the {force: true} parameter if you need to update the models
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log("My-Burger-Sequel is listening on PORT " + port);
+  });
+});
